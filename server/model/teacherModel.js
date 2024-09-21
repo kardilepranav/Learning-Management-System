f3e88328b71c02682138cc1941ef4eb55d84815d
kardilepranav <pranavkardile777@gmail.com>
2024-09-21 14:44:49 +0530

    Created model for teacher

---
+++ server/model/teacherModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const teacherSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 50,
+    unique: true,
+    match: [/\S+@\S+\.\S+/, 'Please enter a valid email address'],
+  },
+  username: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    min: 8,
+    max: 20,
+  },
+  subjects: [],
+})
