bce94c9ae41e58b62613a5fdeb5222df3ed64202
kardilepranav <pranavkardile777@gmail.com>
2024-09-21 14:15:34 +0530

    Created signin controller for admin

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -10,8 +10,8 @@ module.exports.signup = async (req, res, next) => {
 			return res
 				.status(403)
 				.json({ msg: 'Admin already exists', status: false });
-        }
-        
+		}
+
 		const hashedPassword = bcrypt.hashSync(password, 10);
 		const obj = {
 			username,
@@ -36,7 +36,32 @@ module.exports.signup = async (req, res, next) => {
 };
 
 module.exports.signin = async (req, res, next) => {
-	console.log('hello');
+	const { username, password } = req.body;
+	try {
+		const admin = await Admin.findOne({ username });
+		if (!admin) {
+			return res
+				.status(401)
+				.json({ msg: 'Invalid username or password', status: false });
+		}
+
+		const validPassword = bcrypt.compareSync(password, admin.password);
+		if (!validPassword) {
+			return res
+				.status(401)
+				.json({ msg: 'Invalid username or password', status: false });
+		}
+
+		const token = jwt.sign({ username, role: 'admin' }, process.env.SECRET, {
+			expiresIn: '1h',
+		});
+
+		return res
+			.status(200)
+			.json({ msg: 'Signin succesfull', token, status: true });
+	} catch (err) {
+		next(err);
+	}
 };
 
 module.exports.createTeacher = async (req, res, next) => {
