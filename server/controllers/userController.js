9ce6b20807a53f2a3cc84014a1e5c42b0bd4d7dd
kardilepranav <pranavkardile777@gmail.com>
2024-10-13 19:50:05 +0530

    Created change password functionality for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,3 +34,43 @@ module.exports.signin = async (req, res) => {
 			.json({ message: 'server error', error, status: false });
 	}
 };
+
+module.exports.changePassword = async (req, res) => {
+	const { currentPassword, newPassword } = req.body;
+	const username = req.user.username;
+	try {
+		const user = await User.findOne({ username });
+		if (!user) {
+			return res.status(404).json({ message: 'User not found', status: false });
+		}
+
+		const isMatch = bcrypt.compareSync(currentPassword, user.password);
+		if (!isMatch) {
+			return res
+				.status(400)
+				.json({ message: 'Incorrect current password', status: false });
+		}
+
+		if (currentPassword === newPassword) {
+			return res.status(403).json({
+				message: 'Password should not be same as current password',
+				status: false,
+			});
+		}
+    delete req.body.currentPassword;
+
+    const hashedPassword = bcrypt.hashSync(newPassword, 10);
+    user.password = hashedPassword;
+    user.save();
+
+    delete req.body.newPassword;
+    
+		return res
+			.status(200)
+			.json({ message: 'Password changed successfully', status: true });
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ message: 'server error', error, status: false });
+	}
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
-const { signin } = require('../controllers/userController');
+const authorizeJwt = require('../auth');
+const { signin, changePassword } = require('../controllers/userController');
 
 const router = require('express').Router();
 
 router.post('/signin', signin);
-
+router.put('/change-password', authorizeJwt, changePassword);
 
 module.exports = router;
